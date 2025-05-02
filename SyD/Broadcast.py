d5602d25b88b97030ec9a973d0db472e89d1c549
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-02 16:00:54 +0530

    Update Broadcast.py

--- SyD/Broadcast.py
+++ SyD/Broadcast.py
@@ -17,6 +17,7 @@ async def give_filter(client, message):
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
     await message.reply_text("Bᴏᴛ ꜱᴛᴏᴩᴩᴇᴅ! 🙃")
+    await message.reply_text("<i>Dɪꜱᴄʟᴀɪᴍᴇʀ!: ᴛʜɪꜱ ʙᴏᴛ ᴅᴏ ɴᴏᴛ ᴏᴡɴ ᴀɴʏ ꜰɪʟᴇꜱ.</i>")
     await message.reply_text(
          text="<b>Pʟᴇᴀꜱᴇ ꜱᴇɴᴅ ʜᴇʀᴇ,🥹 @Movies_Forage_Bot</b>",   
          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🥶 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 🥶", url=f"https://t.me/+5n7vViwKXJJiMjhl")]])
