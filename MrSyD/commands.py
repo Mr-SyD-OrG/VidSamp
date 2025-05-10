5e07de5bf7b092f03eb26342c0825df0726f144b
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-10 08:50:20 +0530

    Create commands.py

---
+++ MrSyD/commands.py
@@ -0,0 +1,12 @@
+from info import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from pyrogram import Client, filters, enums
+
+
+
+@Client.on_message(filters.command("start") & filters.incoming)
+async def start(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    
