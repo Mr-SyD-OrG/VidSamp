1d3ffaa8035e92390d5e63800e78662e92f26497
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-05-10 09:04:17 +0530

    Update commands.py

--- MrSyD/commands.py
+++ MrSyD/commands.py
@@ -10,3 +10,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     
+    await message.reply_text(
+        text="<b>Oᴋ ʏᴏᴜ ᴄᴀɴ ᴄᴏɴᴛɪɴᴜᴇ ᴜꜱɪɴɢ ᴛʜᴇ ʙᴏᴛ...!😊 \nTʜᴀɴᴋꜱ ꜰᴏʀ ᴛʜᴇ ᴩʟᴇᴀꜱᴜʀᴇ ✨ @Mr_MovSearch_Bot</b>",   
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("OPEN", url=f"https://t.me/+5n7vViwKXJJiMjhl")]])
+    )
