2dfdb1f97d126de3ccdb4d9fa891d0664f0d88eb
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-06-07 12:39:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mr-SyD-OrG/FileLin.git /TheMovieProviderBot
+  git clone https://github.com/Mr-SyD-OrG/VidSamp.git /TheMovieProviderBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TheMovieProviderBot
